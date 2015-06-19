73eb67155c6ca222fe9bd251766610c070805bfa
Peak Xu <peak.xu@gmail.com>
2015-06-19 16:11:05 -0400

    Factor out build directory variable

--- lib/dynamo-local-ruby/dynamo_db_local.rb
+++ lib/dynamo-local-ruby/dynamo_db_local.rb
@@ -5,8 +5,6 @@ module DynamoLocalRuby
   class DynamoDBLocal
     PORT = 9389
     ENDPOINT = "http://localhost:#{PORT}"
-    DYNAMO_LOCAL_PATH = File.expand_path('../../../build/dynamo_db_local',
-                                         __FILE__)
     LATEST_WEB_TGZ = 'http://dynamodb-local.s3-website-us-west-2.amazonaws.com'\
                      '/dynamodb_local_latest.tar.gz'
 
@@ -15,19 +13,20 @@ module DynamoLocalRuby
     end
 
     class << self
-      def ensure_dynamo_local_exists
-        jar_path = File.join(DYNAMO_LOCAL_PATH, 'DynamoDBLocal.jar')
+      def ensure_dynamo_local_exists(local_path)
+        jar_path = File.join(local_path, 'DynamoDBLocal.jar')
         return if File.exist?(jar_path)
-        `mkdir -p #{DYNAMO_LOCAL_PATH}`
-        latest = File.join(DYNAMO_LOCAL_PATH, 'dynamodb_local_latest.tar.gz')
+        `mkdir -p #{local_path}`
+        latest = File.join(local_path, 'dynamodb_local_latest.tar.gz')
         `wget #{LATEST_WEB_TGZ} -o #{latest}` unless File.exist?(latest)
-        `tar xzf #{latest} -C #{DYNAMO_LOCAL_PATH}`
+        `tar xzf #{latest} -C #{local_path}`
       end
 
-      def up
-        ensure_dynamo_local_exists
-        lib_path = File.join(DYNAMO_LOCAL_PATH, 'DynamoDBLocal_lib')
-        jar_path = File.join(DYNAMO_LOCAL_PATH, 'DynamoDBLocal.jar')
+      def up(build_path)
+        local_path = File.join(build_path, 'dynamo_db_local')
+        ensure_dynamo_local_exists(local_path)
+        lib_path = File.join(local_path, 'DynamoDBLocal_lib')
+        jar_path = File.join(local_path, 'DynamoDBLocal.jar')
         pid = spawn("java -Djava.library.path=#{lib_path} -jar #{jar_path} "\
                     "-sharedDb -inMemory -port #{PORT}")
         @instance = DynamoDBLocal.new(pid)
